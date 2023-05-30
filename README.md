2e19ae9e0b9ba5f7ebf37c66d70f4842b7439f37
Ashish-Rayalwar <119595991+Ashish-Rayalwar@users.noreply.github.com>
2023-05-30 16:00:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-a4mxeu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-a4mxeu)
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  let [count, setCount] = useState(0);
+
+  let Incr = () => {
+    setCount((c) => c + 1);
+  };
+  let IncrByTen = () => {
+    setCount((c) => c + 10);
+  };
+  let DecrByTen = () => {
+    setCount((c) => c - 10);
+  };
+  let Decr = () => {
+    setCount((c) => c - 1);
+  };
+
+  return (
+    <div class="container">
+      <button onClick={DecrByTen} class="left-btn">
+        left
+      </button>
+      <div class="main">
+        <div onClick={Incr} class="arrow-up"></div>
+        <div class="count">{count}</div>
+        <div onClick={Decr} class="arrow-down"></div>
+      </div>
+      <button onClick={IncrByTen} class="right-btn">
+        right
+      </button>
+    </div>
+  );
+}
